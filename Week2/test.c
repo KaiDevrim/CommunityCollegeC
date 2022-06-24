282222a440a7f7a6edafccb6546e90f0bdd07425
Kai Devrim <kai@devrim.tech>
2022-06-23 22:23:47 -0700

    Finish Week 1, start work on Week 2

---
+++ Week2/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int i = 1;
+int months = 5;
+int futureValue = 0;
+int monthlyInterestRate = 0;
+while (i < months)
+{
+    printf("%i", i);
+    i = i + 1;
+}
+    return 0;
+}
